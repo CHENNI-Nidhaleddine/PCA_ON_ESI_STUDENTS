c74c40cc6bfdde0d10571dde0c74f95aae643410
nid_ch <ne.chenni@gmail.com>
2022-12-29 19:05:35 +0100

    done

---
+++ src/main.py
@@ -0,0 +1,48 @@
+import utils
+import numpy as np 
+
+# NIDHALEDDINE CHENNI FROM SID
+
+def main():
+
+    path='data/PV1CSS2.xlsx'
+    axe_x=0
+    axe_y=1
+
+    print("Reading and processing data ...")
+    data=utils.readData(path)
+    data=utils.Preprocess(data)
+    utils.writeData(data,path)
+    print("Data is ready ...")
+
+    print("Performing PCA ...")
+    # Coef multiplication
+    data=np.asarray(data)
+    data[:,0]=data[:,0]*5
+    data[:,1]=data[:,1]*5
+    data[:,2]=data[:,2]*1
+    data[:,3]=data[:,3]*3
+    data[:,4]=data[:,4]*4
+    data[:,5]=data[:,5]*2
+    data[:,6]=data[:,6]*4
+    data[:,7]=data[:,7]*5
+    data[:,8]=data[:,8]*5
+    
+    # lambdas: eigen values
+    # Q: eigen vectors
+    # Z: individuals projection
+    # V: variables projection
+    lambdas,Q,Z,V=utils.PCA(data)
+
+    print("PCA finished ...")
+
+    utils.plot_eigen_values(lambdas)
+    utils.plot_inertias(lambdas)
+    utils.plot_cum_inertias(lambdas)
+    utils.plot_indv_factor_map(Z,axe_x,axe_y)
+    utils.plot_var_facrot_map(lambdas,Q,utils.SUBJECTS_S2,axe_x,axe_y)
+    
+    print("End ...")
+    
+if __name__=='__main__':
+    main()
---
+++ src/utils.py
@@ -0,0 +1,140 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import sklearn.decomposition
+import re
+
+# NIDHALEDDINE CHENNI FROM SID
+
+# CONSTANTS
+CLASSEMENT=21
+SUBJECTS_S1=np.array(('SYS1','RES1','ANUM','RO','ORG','IGL','THP','LANG1'))
+SUBJECTS_S2=np.array(('MCSI','BDD','SEC','CPROJ','PROJ','LANG2','ARCH','SYS2','RES2'))
+COEF_S1=np.array((5,5,5,3,3,5,4,2))
+COEF_S2=np.array((5,5,1,3,3,2,4,4,4))
+
+# FUNCTIONS
+def readData(path):
+    """ JUST READ DATA FROM EXCEL :) """
+    data=pd.read_excel(path)
+    return data
+    
+def writeData(data,path):
+    """ JUST WRITE DATA TO EXCEL :) """
+    data.to_excel(path,index=False)
+
+def Preprocess(data):
+    """ TRAIT CELLS LIKE (ANAD 4.5 < 5.6) WILL BECOME JUST 4.5 """
+    data.columns=SUBJECTS_S2
+    
+    for row in range(data.shape[0]):
+        for col in range(data.shape[1]):
+            value=data.iloc[row,col]
+            try:
+                float(value)
+            except:
+                if(value==''):
+                    data.iloc[row,col]=0
+                else:
+                    floats=re.findall("\d\.\d\d", value)
+                    data.iloc[row,col]=floats[0]
+    return data
+
+
+def Cov(X):
+    N=X.shape[0]
+    meanX=X.mean()
+    return (np.dot((X-meanX).T,X-meanX))/N
+
+def PCA(X):
+    X=np.asarray(X)*COEF_S2
+    # center and normalize data
+    STD=np.std(X,axis=0)
+    M=np.mean(X,axis=0)
+    X=X-M
+    X=X/STD
+
+    # Calculate eigen values and eigen vectors
+    covX=np.cov(X.T)
+    lambdas, Q= np.linalg.eigh(covX)
+  
+    # Order from biggest to smallest
+    idx=np.argsort(-lambdas)
+    lambdas=lambdas[idx]
+    lambdas=np.maximum(lambdas,0) # in case there are some negative close to zero
+    Q=Q[:,idx]
+    
+    # project the data
+    Z=X.dot(Q)
+
+    # project variables
+    V=-np.sqrt(lambdas)*Q
+
+    return lambdas,Q,Z,V
+
+def plot_eigen_values(lambdas):
+    plt.scatter(np.arange(1,lambdas.shape[0]+1),lambdas)
+    plt.title('les valeurs propres')
+    plt.show()
+
+def plot_inertias(lambdas):
+    plt.plot(lambdas/sum(lambdas))
+    plt.title('les inerties')
+    plt.show()
+
+def plot_cum_inertias(lambdas):
+    plt.plot(np.cumsum(lambdas/sum(lambdas)))
+    plt.title('les inerties cumelee')
+    plt.show()
+
+def plot_indv_factor_map(Z,i=0,j=1):
+    arr=np.zeros(Z.shape[0])+0.7
+    arr[CLASSEMENT-1]=1 #my rank point with different color
+    plt.scatter(-Z[:,i],-Z[:,j],c=arr,alpha=arr)
+    plt.title('individuals factor map for axes ')
+    axex='dim '+str(i+1)
+    axey='dim '+str(j+1)
+    plt.xlabel(axex)
+    plt.ylabel(axey)
+    plt.axhline()
+    plt.axvline()
+    plt.show()
+
+
+
+def plot_var_facrot_map(lambdas,Q,labels,axe_x,axe_y):
+
+    # calculate variable projection
+    V=-np.sqrt(lambdas)*Q
+    print("variable projection: ",V)
+
+    figsize=(8,8)
+    #Plot
+    plt.figure(figsize=figsize)
+    plt.rcParams.update({'font.size': 12}) 
+    
+    #Plot circle
+    x=np.linspace(start=-1,stop=1,num=500)
+    y_positive=lambda x: np.sqrt(1-x**2) 
+    y_negative=lambda x: -np.sqrt(1-x**2)
+    plt.plot(x,list(map(y_positive, x)), color='maroon')
+    plt.plot(x,list(map(y_negative, x)),color='maroon')
+
+    #Plot smaller circle
+    x=np.linspace(start=-0.5,stop=0.5,num=500)
+    y_positive=lambda x: np.sqrt(0.5**2-x**2) 
+    y_negative=lambda x: -np.sqrt(0.5**2-x**2)
+    plt.plot(x,list(map(y_positive, x)), color='maroon')
+    plt.plot(x,list(map(y_negative, x)),color='maroon')
+
+    #Create broken lines
+    x=np.linspace(start=-1,stop=1,num=30)
+    plt.scatter(x,[0]*len(x), marker='_',color='maroon')
+    plt.scatter([0]*len(x), x, marker='|',color='maroon')
+    for i in range(V.shape[0]):
+        plt.text(V[i,axe_x],V[i,axe_y], s=labels[i] )
+        plt.arrow(0,0, dx=V[i,axe_x], dy=V[i,axe_y], head_width=0.03, head_length=0.03,  length_includes_head=True)
+
+    plt.xlabel(f"Dim {axe_x+1} ({round(lambdas[axe_x]/np.sum(lambdas)*100,2)}%)")
+    plt.ylabel(f"Dim {axe_y+1} ({round(lambdas[axe_y]/np.sum(lambdas)*100,2)}%)")
+    plt.show()
